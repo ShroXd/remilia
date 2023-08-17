5f41cef3f05d71b0eb2cdc8ff72ec9fe543cd5da
Atriiy <ShroXdF@gmail.com>
2023-08-17 23:09:50 +0800

    WIP: wrap http request

--- pkg/network/request.go
+++ pkg/network/request.go
@@ -3,51 +3,19 @@ package network
 import (
 	"io"
 	"net/http"
-	"remilia/pkg/logger"
-
-	"go.uber.org/zap"
 )
 
 type Request struct {
-	Header *http.Header
-	Client *http.Client
-	URL    string
-	Method string
+	internal *http.Request
 }
 
-func New(method, URL string, header *http.Header) (*Request, error) {
-	client := &http.Client{}
+func New(method, url string, body io.Reader) (*Request, error) {
+	r, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, err
+	}
 
 	return &Request{
-		URL:    URL,
-		Header: header,
-		Client: client,
-		Method: method,
+		internal: r,
 	}, nil
-}
-
-func (r *Request) Visit() string {
-	req, err := http.NewRequest(r.Method, r.URL, nil)
-	if err != nil {
-		logger.Error("Error creating request", zap.Error(err))
-	}
-
-	req.Header = *r.Header
-
-	resp, err := r.Client.Do(req)
-	if err != nil {
-		logger.Error("Error sending request", zap.Error(err))
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		logger.Error("Unexpected response status code", zap.Error(err))
-	}
-
-	bodyBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		logger.Error("Failed to read response body", zap.Error(err))
-	}
-
-	return string(bodyBytes)
 }
