fc0bfb45ef6919c7bfca2683f3097b644fb6e12e
Atriiy <ShroXdF@gmail.com>
2023-08-17 20:36:10 +0800

    test: add benchmark for FanOut

--- pkg/concurrency/multiple_bench_test.go
+++ pkg/concurrency/multiple_bench_test.go
@@ -2,21 +2,21 @@ package concurrency
 
 import "testing"
 
+func generateDate(n int) <-chan int {
+	out := make(chan int)
+
+	go func() {
+		defer close(out)
+		for i := 0; i < n; i++ {
+			out <- i
+		}
+	}()
+
+	return out
+}
+
 func BenchmarkFanIn(b *testing.B) {
 	b.ReportAllocs()
-
-	generateDate := func(n int) <-chan int {
-		out := make(chan int)
-
-		go func() {
-			defer close(out)
-			for i := 0; i < n; i++ {
-				out <- i
-			}
-		}()
-
-		return out
-	}
 
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
@@ -29,6 +29,26 @@ func BenchmarkFanIn(b *testing.B) {
 		b.StartTimer()
 
 		out := FanIn(done, ch1, ch2, ch3)
+
+		for range out {
+		}
+		close(done)
+	}
+}
+
+func BenchmarkFanOut(b *testing.B) {
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+
+		input := generateDate(1000)
+		done := make(chan struct{})
+		fn := func(raw int) int { return raw }
+
+		b.StartTimer()
+
+		out := FanOut(done, input, 1000, fn)
 
 		for range out {
 		}
