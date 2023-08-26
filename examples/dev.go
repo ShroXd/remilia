ceea9ffa113ee908d37ca915adaaaeaf0781660c
Atriiy <ShroXdF@gmail.com>
2023-08-26 12:12:16 +0800

    chore: update example

--- examples/dev.go
+++ examples/dev.go
@@ -3,27 +3,24 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/url"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/ShroXd/remilia"
 )
 
 func main() {
-	scraper := remilia.New("https://www.23qb.net/lightnovel/", remilia.ConcurrentNumber(1))
-
-	scraper.UseURL(".pagelink a", func(s *goquery.Selection) *url.URL {
-		href, _ := s.Attr("href")
-		url, _ := url.Parse(href)
-
-		return url
-	}).UseHTML("h3 a", func(s *goquery.Selection) interface{} {
+	htmlParser := func(s *goquery.Selection) interface{} {
 		return s.Text()
-	}, func(data <-chan interface{}) {
+	}
+
+	contentConsumer := func(data <-chan interface{}) {
 		for v := range data {
-			fmt.Println("Get data in data consumer: ", v)
+			fmt.Println("Receive data: ", v)
 		}
-	}).AddToChain()
+	}
+
+	scraper := remilia.New("https://go.dev/", remilia.ConcurrentNumber(10))
+	scraper.UseHTML(".WhyGo-reasonText p", htmlParser, contentConsumer).AddToChain()
 
 	err := scraper.Start()
 	if err != nil {
